09bf4ffcd056ff958fe88ddcad30c7aa09e015d7
Lilanka Udawatta <lilanka@g-labs.digital>
2023-02-28 23:56:38 -0500

    initial commit

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -10,6 +10,18 @@ export const fetchData = async (location, queryDate, token) => {
     try{
         const res = await fetch(url, {
                 method: 'GET',
+                body: undefined, // string, FormData, Blob, BufferSource, or URLSearchParams
+                referrer: "about:client", // or "" to send no Referer header,
+                // or an url from the current origin
+                referrerPolicy: "strict-origin-when-cross-origin", // no-referrer-when-downgrade, no-referrer, origin, same-origin...
+                mode: "cors", // same-origin, no-cors
+                credentials: "same-origin", // omit, include
+                cache: "default", // no-store, reload, no-cache, force-cache, or only-if-cached
+                redirect: "follow", // manual, error
+                integrity: "", // a hash, like "sha256-abcdef1234567890"
+                keepalive: false, // true
+                signal: undefined, // AbortController to abort request
+                window: window // null 
             })
         
             
--- src/api/getAPIKey.js
+++ src/api/getAPIKey.js
@@ -6,7 +6,20 @@ export const getAPIKey = async () =>{
     
     try{
         const res = await fetch('https://login.meteomatics.com/api/v1/token', {
-            method: 'GET', headers: headers
+            method: 'GET', 
+            headers: headers,
+            body: undefined, // string, FormData, Blob, BufferSource, or URLSearchParams
+            referrer: "about:client", // or "" to send no Referer header,
+            // or an url from the current origin
+            referrerPolicy: "strict-origin-when-cross-origin", // no-referrer-when-downgrade, no-referrer, origin, same-origin...
+            mode: "cors", // same-origin, no-cors
+            credentials: "same-origin", // omit, include
+            cache: "default", // no-store, reload, no-cache, force-cache, or only-if-cached
+            redirect: "follow", // manual, error
+            integrity: "", // a hash, like "sha256-abcdef1234567890"
+            keepalive: false, // true
+            signal: undefined, // AbortController to abort request
+            window: window // null  
         })
 
         const data = await res.json()
